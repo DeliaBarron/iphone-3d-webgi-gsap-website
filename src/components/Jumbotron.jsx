f43638398799aea6c65bbb8d82cf055c7abd0877
DeliaBarron <deliabarron.db@gmail.com>
2023-04-10 17:05:28 -0600

    DisplaySection

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -6,7 +6,7 @@ const Jumbotron = () => {
     const handleLearnMore = ()=>{
         const element = document.querySelector('.sound-section')
         window.scrollTo({
-            top:element.getBoundingClientRect().top, //top position
+            top: element?.getBoundingClientRect().top, //top position
             left:0,
             behavior:'smooth'
         })
