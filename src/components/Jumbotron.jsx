2a142a59b6481e03526824fcb61113445788d02f
DeliaBarron <deliabarron.db@gmail.com>
2023-04-10 16:38:58 -0600

    added Sound-section & window.scrollTo() function

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,14 @@ import iPhone from '../assets/images/iphone-14.jpg'
 import HoldingIphone from '../assets/images/iphone-hand.png'
 
 const Jumbotron = () => {
+    const handleLearnMore = ()=>{
+        const element = document.querySelector('.sound-section')
+        window.scrollTo({
+            top:element.getBoundingClientRect().top, //top position
+            left:0,
+            behavior:'smooth'
+        })
+    }
   return (
     <div className='jumbotron-section wrapper'>
         <h2 className='title'>New</h2>
@@ -16,7 +24,7 @@ const Jumbotron = () => {
                 <button className='button'>Buy</button>
             </li>
             <li>
-                <a className='link'>Learn more</a>
+                <a className='link' onClick={handleLearnMore}>Learn more</a>
             </li>
         </ul>
         <img className='iphone-img' src={HoldingIphone} alt="iPhone" />
