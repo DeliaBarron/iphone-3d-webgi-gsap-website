03b1f73b04e4d15259ca8c7a8c420e5a533b07e5
DeliaBarron <deliabarron.db@gmail.com>
2023-04-10 17:49:41 -0600

    Building WebGi viewer component

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,81 @@
+import { React, useRef, useState, useCallback, forwardRef, useImperativeHandle, useEffect } from 'react'
+import {
+    ViewerApp,
+    AssetManagerPlugin,
+    GBufferPlugin,
+    ProgressivePlugin,
+    TonemapPlugin,
+    SSRPlugin,
+    SSAOPlugin,
+    BloomPlugin,
+    GammaCorrectionPlugin,
+    mobileAndTabletCheck
+} from "webgi";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+const WebgiViewer = () => {
+    const canvasRef = useRef(null)
+
+    //initialize 3D model and use it
+    const setupViewer = useCallback(async () => {
+        // Initialize the viewer
+        const viewer = new ViewerApp({
+            canvas: canvasRef.current
+        })
+
+        // Add some plugins.
+        const manager = await viewer.addPlugin(AssetManagerPlugin)
+
+        //for the addEventListener element (accesing the camera, its position and the target via viewer) 
+        //with these 3 variables we can animate our 3D model
+        const camera = viewer.scene.activeCamera
+        const position = camera.position
+        const target = camera.target
+
+        // Add plugins individually.
+         await viewer.addPlugin(GBufferPlugin)
+        await viewer.addPlugin(new ProgressivePlugin(32))
+        await viewer.addPlugin(new TonemapPlugin(true))
+        await viewer.addPlugin(GammaCorrectionPlugin)
+        await viewer.addPlugin(SSRPlugin)
+        await viewer.addPlugin(SSAOPlugin)
+        await viewer.addPlugin(BloomPlugin)
+
+        // This must be called once after all plugins are added.
+        viewer.renderer.refreshPipeline()
+
+        await manager.addFromPath("scene-black.glb")//like that cause we are adding it from the public folder
+        viewer.getPlugin(TonemapPlugin).config.clipBackground=true
+
+        //disable controll once we have our viewers, so the users are
+        // NOT ABLE to rotate the 3D model once the website is loaded
+        viewer.scene.activeCamera.setCameraOptions({ controlsEnabled : false })
+
+        //whenever we reload the website, set the position of the page to be on top
+        window.scrollTo(0,0)//top and left set to 0
+
+        //add event listener to the viewer so it will update the position of the 'camera' 
+        //if update is needed 
+        let needsUpdate = true
+        viewer.addEventListener('preFrame', () => {
+            if(needsUpdate){
+                camera.positionTargetUpdated(true)
+                needsUpdate = false
+            }
+        })
+
+    }, []);//empty dependencies cause we dont want to recreate this funct
+    useEffect(()=>{
+        setupViewer()
+    },[])//to call the funct only once so is empty
+    return (
+        <div id='webgi-canvas-container'>
+            <canvas id='webgi-canvas' ref={canvasRef}>
+
+            </canvas>
+        </div>
+    )
+}
+
+export default WebgiViewer
