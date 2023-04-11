01fc4a2730e23c7be62b9b6d42780c617a21a2d8
DeliaBarron <deliabarron.db@gmail.com>
2023-04-10 21:56:33 -0600

    preview mode and exit preview mode functions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useRef } from "react";
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
 import DisplaySection from "./components/DisplaySection";
 import WebgiViewer from "./components/WebgiViewer";
 function App() {
-
+  const webgiViewerRef = useRef()
+  const contentRef= useRef()
+  //function that will be triggering on DisplaySection component
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview()//accesing the method/funct triggerPreview on webgiViewer
+  }
   return (
     <div className="App">
-      <Nav/>
-      <Jumbotron/>
-      <SoundSection/>
-      <DisplaySection />
-      <WebgiViewer/>
+     <div ref={contentRef} id="content">
+      {/**This ref will fade away all this content once 
+        we are on previewMode triggered by the Try me! button 
+        and only the webGiViewer will be available on the website*/}
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection triggerPreview={handlePreview} />
+     </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -17,14 +17,46 @@ import { scrollAnimation } from '../lib/scroll-animation';
 
 gsap.registerPlugin(ScrollTrigger)
 
-const WebgiViewer = () => {
+const WebgiViewer = forwardRef((props, ref) => {
     const canvasRef = useRef(null)
+    const [viewerRef, setViewerRef] = useState(null)
+    const [targetRef, setTargetRef] = useState(null)
+    const [cameraRef, setCameraRef] = useState(null)
+    const [positionRef, setPositionRef] = useState(null)
+    const canvasContainerRef = useRef(null)
+    const [previewMode, setPreviewMode] = useState(false)
+    //what is the function that pulls the trigger from the App component
+    useImperativeHandle(ref, () => ({
+        triggerPreview() {
+            setPreviewMode(true)
+            canvasContainerRef.current.style.pointerEvents = 'all'
+            props.contentRef.current.style.opacity = '0'
+
+            gsap.to(positionRef, {
+                x: 13.04,
+                y: -2.01,
+                z: 2.29,
+                duration: 2,
+                onUpdate: () => {
+                    viewerRef.setDirty()
+                    cameraRef.positionTargetUpdated(true)//position of the target has been updated
+                }
+            })
+            gsap.to(targetRef, {
+                x: 0.11,
+                y: 0.0,
+                z: 0.0,
+                duration: 2
+            })
+            viewerRef.scene.activeCamera.setCameraOptions({ controlsEnabled: true })
+        }
+    }))
     //add to the cache the scrollAnimation everytime it has to render
-    const memorizedScrollAnimation = useCallback( (position, target, onUpdate) => {
-            if(position && target && onUpdate){
-                scrollAnimation(position, target, onUpdate)
-            }
-        },[]//to execute only once
+    const memorizedScrollAnimation = useCallback((position, target, onUpdate) => {
+        if (position && target && onUpdate) {
+            scrollAnimation(position, target, onUpdate)
+        }
+    }, []//to execute only once
     )
     //initialize 3D model and use it
     const setupViewer = useCallback(async () => {
@@ -32,6 +64,8 @@ const WebgiViewer = () => {
         const viewer = new ViewerApp({
             canvas: canvasRef.current
         })
+        setViewerRef(viewer)
+
 
         // Add some plugins.
         const manager = await viewer.addPlugin(AssetManagerPlugin)
@@ -42,8 +76,12 @@ const WebgiViewer = () => {
         const position = camera.position
         const target = camera.target
 
+        setCameraRef(camera)
+        setPositionRef(position)
+        setTargetRef(target)
+
         // Add plugins individually.
-         await viewer.addPlugin(GBufferPlugin)
+        await viewer.addPlugin(GBufferPlugin)
         await viewer.addPlugin(new ProgressivePlugin(32))
         await viewer.addPlugin(new TonemapPlugin(true))
         await viewer.addPlugin(GammaCorrectionPlugin)
@@ -55,25 +93,25 @@ const WebgiViewer = () => {
         viewer.renderer.refreshPipeline()
 
         await manager.addFromPath("scene-black.glb")//like that cause we are adding it from the public folder
-        viewer.getPlugin(TonemapPlugin).config.clipBackground=true
+        viewer.getPlugin(TonemapPlugin).config.clipBackground = true
 
         //disable controll once we have our viewers, so the users are
         // NOT ABLE to rotate the 3D model once the website is loaded
-        viewer.scene.activeCamera.setCameraOptions({ controlsEnabled : false })
+        viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false })
 
         //whenever we reload the website, set the position of the page to be on top
-        window.scrollTo(0,0)//top and left set to 0
+        window.scrollTo(0, 0)//top and left set to 0
 
         //add event listener to the viewer so it will update the position of the 'camera' 
         //if update is needed 
         let needsUpdate = true
-        const onUpdate = ()=>{
+        const onUpdate = () => {
             //we're saying the camera and the viewer need to be updated
-            needsUpdate=true
+            needsUpdate = true
             viewer.setDirty()
         }
         viewer.addEventListener('preFrame', () => {
-            if(needsUpdate){
+            if (needsUpdate) {
                 camera.positionTargetUpdated(true)
                 needsUpdate = false
             }
@@ -82,16 +120,56 @@ const WebgiViewer = () => {
 
 
     }, []);//empty dependencies cause we dont want to recreate this funct
-    useEffect(()=>{
+    useEffect(() => {
         setupViewer()
-    },[])//to call the funct only once so is empty
-    return (
-        <div id='webgi-canvas-container'>
-            <canvas id='webgi-canvas' ref={canvasRef}>
+    }, [])//to call the funct only once so is empty
 
-            </canvas>
+    const handleExit = useCallback(() => {
+        // reset back
+        canvasContainerRef.current.style.pointerEvents = 'none'
+        props.contentRef.current.style.opacity = '1'
+        viewerRef.scene.activeCamera.setCameraOptions({ controlsEnabled: false })
+        setPreviewMode(false)
+        gsap.to(positionRef, {
+            x: 1.56,
+            y: 5.0,
+            z: 0.01,
+            scrollTrigger: {
+                trigger: '.display-section', //the last section (display) will be the trigger now 
+                start: 'top bottom',
+                end: 'top top',
+                scrub: 2,
+                immediateRender: false
+            },
+            onUpdate:()=>{
+                viewerRef.setDirty()
+                cameraRef.positionTargetUpdated(true)
+            }
+        })
+        gsap.to(targetRef, {
+            x: -0.55,
+            y: 0.32,
+            z: 0.0,
+            scrollTrigger: {
+                trigger: '.display-section',
+                start: 'top bottom',
+                end: 'top top',
+                scrub: 2,
+                immediateRender: false
+            }
+        })
+    }, [canvasContainerRef, viewerRef, positionRef, cameraRef, targetRef])
+    return (
+        <div ref={canvasContainerRef} id='webgi-canvas-container'>
+            <canvas id='webgi-canvas' ref={canvasRef}></canvas>
+            {
+                previewMode && (
+                    <button className='button' onClick={handleExit}>Exit</button>
+                )
+            }
         </div>
     )
-}
+
+})
 
 export default WebgiViewer
